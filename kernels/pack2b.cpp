e8791bf3c9ed0c21fbc9aa58e10b957e3a814401
oliver grainge <oliver@grainge.me>
2024-12-15 20:50:57 +0000

    adding setup.py

--- kernels/pack2b.cpp
+++ kernels/pack2b.cpp
@@ -1,9 +1,12 @@
 #include "pack2b_cpu.h"
+#ifdef USE_CUDA
+#include "pack2b_cuda.h"
+#endif
 #include <torch/extension.h>
 
 torch::Tensor pack2b(torch::Tensor A)
 {
-    TORCH_CHECK(A.device().is_cpu(), "Tensor 'A' must be on CPU");
+    // Common checks for both CPU and CUDA implementations
     TORCH_CHECK(A.dtype() == torch::kInt8, "Tensor 'A' must be of type int8");
     TORCH_CHECK(A.dim() == 2, "Tensor 'A' must be 2-dimensional");
     TORCH_CHECK(A.size(1) % 4 == 0, "Width must be a multiple of 4");
@@ -14,8 +17,25 @@ torch::Tensor pack2b(torch::Tensor A)
     int64_t N = A.size(1);
     int64_t packed_cols = N / 4;
 
-    torch::Tensor C = torch::zeros({M, packed_cols}, torch::dtype(torch::kInt8).device(torch::kCPU));
-    _pack2b_cpu(A.data_ptr<int8_t>(), M, N, C.data_ptr<int8_t>());
-
-    return C;
+    // Check device and use appropriate implementation
+    if (A.device().is_cuda())
+    {
+#ifdef USE_CUDA
+        torch::Tensor C = torch::zeros({M, packed_cols}, torch::dtype(torch::kInt8).device(torch::kCUDA));
+        _pack2b_cuda(A.data_ptr<int8_t>(), M, N, C.data_ptr<int8_t>());
+        return C;
+#else
+        TORCH_CHECK(false, "CUDA implementation not available. Recompile with USE_CUDA defined.");
+#endif
+    }
+    else if (A.device().is_cpu())
+    {
+        torch::Tensor C = torch::zeros({M, packed_cols}, torch::dtype(torch::kInt8).device(torch::kCPU));
+        _pack2b_cpu(A.data_ptr<int8_t>(), M, N, C.data_ptr<int8_t>());
+        return C;
+    }
+    else
+    {
+        TORCH_CHECK(false, "Tensor 'A' must be on CPU or CUDA.");
+    }
 }
--- setup.py
+++ setup.py
@@ -1,6 +1,67 @@
 from setuptools import setup, find_packages
-from torch.utils.cpp_extension import CppExtension, BuildExtension
-import os 
+from torch.utils.cpp_extension import CppExtension, CUDAExtension, BuildExtension
+import os
+
+def check_cuda_available():
+    try:
+        import torch
+        return torch.cuda.is_available()
+    except ImportError:
+        return False
+
+# Determine whether to build with CUDA support
+use_cuda = check_cuda_available()
+
+# Define sources and extensions
+common_sources = [
+    'kernels/common/tgemm_cpu.cpp',
+    'kernels/common/pack2b_cpu.cpp',
+    'kernels/common/unpack2b_cpu.cpp',
+    'kernels/pack2b.cpp',
+    'kernels/unpack2b.cpp',
+    'kernels/tlinear_forward.cpp',
+    'kernels/bindings.cpp',
+]
+
+cuda_sources = [
+    'kernels/cuda/pack2b_cuda.cu',
+    'kernels/cuda/unpack2b_cuda.cu',
+]
+
+extensions = []
+
+# Add CPU extension
+extensions.append(
+    CppExtension(
+        name='ternify.tnn.functional',
+        sources=common_sources,
+        include_dirs=[
+            os.path.abspath('kernels'),
+            os.path.abspath('kernels/common'),
+        ],
+        extra_compile_args=['-std=c++17', '-fopenmp'],
+        extra_link_args=['-lgomp'],
+    )
+)
+
+# Add CUDA extension if CUDA is available
+if use_cuda:
+    extensions.append(
+        CUDAExtension(
+            name='ternify.tnn.functional_cuda',
+            sources=common_sources + cuda_sources,
+            include_dirs=[
+                os.path.abspath('kernels'),
+                os.path.abspath('kernels/common'),
+                os.path.abspath('kernels/cuda'),
+            ],
+            extra_compile_args={
+                'cxx': ['-std=c++17', '-DUSE_CUDA', '-fopenmp'],  # Add USE_CUDA macro for the CPU part
+                'nvcc': ['-DUSE_CUDA'],  # Add USE_CUDA macro for the CUDA part
+            },
+            extra_link_args=['-lgomp'],
+        )
+    )
 
 setup(
     name='ternify',  # Package name
@@ -8,26 +69,7 @@ setup(
     description='Ternary Neural Network Extensions',
     packages=find_packages(),  # Automatically find and include all packages
     package_dir={"": "."},  # Root directory
-    ext_modules=[
-        CppExtension(
-            name='ternify.tnn.functional',  # Full module path
-            sources=[
-                'kernels/common/tgemm_cpu.cpp',
-                'kernels/common/pack2b_cpu.cpp',
-                'kernels/common/unpack2b_cpu.cpp',
-                'kernels/pack2b.cpp',
-                'kernels/unpack2b.cpp',
-                'kernels/tlinear_forward.cpp',
-                'kernels/bindings.cpp',
-            ],
-            include_dirs=[
-                os.path.abspath('kernels'),
-                os.path.abspath('kernels/common'),
-            ],
-            extra_compile_args=['-std=c++17', '-fopenmp'],
-            extra_link_args=['-lgomp'],
-        )
-    ],
+    ext_modules=extensions,
     cmdclass={'build_ext': BuildExtension},
     zip_safe=False,
 )
